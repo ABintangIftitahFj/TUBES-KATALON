e6fcb1d4ca38757636120fecd6b6d6dea191cc21
ABintangIftitahFj <bintangiftitahf28@upi.edu>
2025-12-14 00:56:57 +0700

    fix: update Katalon test suite path from `TS_Regression_Full` to `TS_Full_Regression` in `run_full_regression.bat`.

--- run_full_regression.bat
+++ run_full_regression.bat
@@ -7,7 +7,7 @@ set KATALON_PATH="C:\Users\HP\.katalon\packages\KSE-10.4.2\katalonc.exe"
 set PROJECT_PATH="c:\KULIAH\SEMESTER 5\SQA\TUBES KATALON"
 
 echo Starting Full Regression Test Suite...
-%KATALON_PATH% -noSplash -runMode=console -projectPath=%PROJECT_PATH% -retry=0 -testSuitePath="Test Suites/TS_Regression_Full" -executionProfile="default" -browserType="Chrome"
+%KATALON_PATH% -noSplash -runMode=console -projectPath=%PROJECT_PATH% -retry=0 -testSuitePath="Test Suites/TS_Full_Regression" -executionProfile="default" -browserType="Chrome"
 
 echo.
 echo Test execution completed!
